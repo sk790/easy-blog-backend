55f2b33f5b3028b61b42c1622fae513786c671ca
sk790 <saurabhkumar16333@gmail.com>
2025-01-03 09:25:49 +0530

    change cors policy

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "*",
+    origin: ["http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
